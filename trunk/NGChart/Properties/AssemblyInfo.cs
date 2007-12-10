f9ac2499fae3b6425bfb7058291990dc2848e208
erymski <erymski@6a9499b6-b140-0410-8933-672d1753f5b6>
2007-12-10 21:58:29 +0000

    Version updated.

--- trunk/NGChart/Properties/AssemblyInfo.cs
+++ trunk/NGChart/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("b610a87f-a159-4497-8338-d3fb5b4702e8")]
 
-[assembly: AssemblyVersion("0.3.*")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
+[assembly: AssemblyVersion("0.4.*")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
